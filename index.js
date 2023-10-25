47de635fd7cdd070e8f1910da45459aad538b309
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2023-10-25 11:44:29 +0200

    chore: Add server code to index.js #69

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World!');
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.end('Hello World');
+});
+
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
