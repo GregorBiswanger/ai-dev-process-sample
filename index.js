f7a32ad007e7ae2e5ff12454b28add7f0c2846c7
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2023-10-30 15:48:23 +0100

    feat: Add index.js file

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('<h1>Hello World</h1>');
+    response.end();
+});
+
+server.listen(3000, () => {
+    console.log('Server läuft auf Port 3000');
+});
