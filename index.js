a9d64c3286311c7419d17bad0a2bc503f9dc8a72
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2023-10-30 15:56:01 +0100

    chore: add error handling

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.write('<h1>Hello World</h1>');
-    response.end();
+    try {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.write('<h1>Hello World</h1>');
+        response.end();
+    } catch (error) {
+        console.error(error);
+        response.writeHead(500, { 'Content-Type': 'text/plain' });
+        response.write('Internal Server Error');
+        response.end();
+    }
 });
 
 server.listen(3000, () => {
