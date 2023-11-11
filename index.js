b37b3d88655a4f2cb92041f5c89410a2e3e56dc0
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2023-11-11 23:43:11 +0100

    Add error handling and update response format in handleIncomingRequest function

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const httpModule = require('http');
 
 const handleIncomingRequest = (request, response) => {
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello World\n');
+  if (request.method !== 'GET') {
+    response.statusCode = 400;
+    response.end('Invalid request method.\n');
+    return;
+  }
+
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'application/json');
+  response.end(JSON.stringify({ message: 'Hello World' }));
 };
 
 const server = httpModule.createServer(handleIncomingRequest);
 
 const SERVER_PORT = 3000;
 server.listen(SERVER_PORT, () => console.log(`Server running at http://localhost:${SERVER_PORT}/`));
+
+process.on('uncaughtException', (err) => {
+  console.error('There was an uncaught error', err);
+  process.exit(1);
+});
