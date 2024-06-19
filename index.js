d889103a4469d10f93d29d8623853cba3b020808
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2024-06-20 01:06:47 +0200

    Add developer jokes service

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const http = require('http');
 
+// Array of developer jokes
+const jokes = [
+  "Why do programmers prefer dark mode? Because light attracts bugs.",
+  "Why do Java developers wear glasses? Because they can't C#.",
+  "How many programmers does it take to change a light bulb? None, that's a hardware problem.",
+  "Why do programmers hate nature? It has too many bugs.",
+  "Why was the JavaScript developer sad? Because he didn't know how to Java."
+];
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  if (req.url === '/jokes') {
+    const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(randomJoke);
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+  }
 });
 
 const PORT = 3000;
