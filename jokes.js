928e4fd9b863db8c0eb8c96c2f384e936c9c5667
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2025-12-03 01:18:52 +0100

    chore: initialize project with package.json and express dependency

---
+++ jokes.js
@@ -0,0 +1,22 @@
+// jokes.js
+module.exports = [
+  {
+    id: 1,
+    text: 'Warum können Elefanten nicht fliegen? Weil sie zu schwer für den Flugzeugmodus sind!',
+  },
+  { id: 2, text: 'Was macht ein Pirat am Computer? Er drückt die Enter-Taste!' },
+  {
+    id: 3,
+    text: 'Warum können Geister so schlecht lügen? Weil man durch sie hindurchsieht!',
+  },
+  { id: 4, text: 'Wie nennt man einen Bumerang, der nicht zurückkommt? Ein Stock.' },
+  { id: 5, text: 'Was ist orange und läuft durch den Wald? Eine Wanderine.' },
+  { id: 6, text: 'Warum dürfen Geier nicht heiraten? Weil sie Aasfresser sind!' },
+  { id: 7, text: 'Was macht ein Keks unter einem Baum? Krümel!' },
+  {
+    id: 8,
+    text: 'Wie nennt man einen Hund ohne Beine? Ist doch egal, er kommt eh nicht!',
+  },
+  { id: 9, text: 'Warum können Seeräuber den Kreis nicht berechnen? Weil sie Pi raten!' },
+  { id: 10, text: 'Was macht ein Pirat im Fitnessstudio? Er trainiert seine Bizeps!' },
+];
